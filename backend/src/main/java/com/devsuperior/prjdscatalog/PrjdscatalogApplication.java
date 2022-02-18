0ab0554859652c2f7e65c7c2497ad5bdd28ec500
Adriano Jacinto <adriano.jacinto@gmail.com>
2022-02-18 06:56:09 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/prjdscatalog/PrjdscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.prjdscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjdscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjdscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/prjdscatalog/PrjdscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.prjdscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjdscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
